7e8c293417e710cd80e435f176212ceb811325f4
mshr <mshr@localhost.localdomain>
2016-09-10 22:51:55 +0900

    configをjsonからモジュールに変更

--- facebook_bot.js
+++ facebook_bot.js
@@ -1,16 +1,14 @@
-import fs from 'fs';
 import moment from 'moment';
 import FacebookBot from './lib/FacebookBot';
 import FacebookFeed from './lib/FacebookFeed';
 import Slack from './lib/Slack';
+import {config} from './config';
 
 async function main() {
   try {
-    let config  = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
-
     let now = moment();
     let since = now.clone()
-          .add(-1, 'hours')
+          .add(-config.repeate.interval, config.repeate.unit)
           .minutes(0)
           .seconds(0)
           .milliseconds(0)
@@ -21,14 +19,14 @@ async function main() {
           .milliseconds(0)
           .unix();
 
-    let res = await new FacebookBot(config).requestFeed(since, until);
+    let res = await new FacebookBot().requestFeed(since, until);
 
     let messages = [];
     res.feed.data.forEach(jsonFeed => {
       messages.push(new FacebookFeed(jsonFeed).json2message());
     });
 
-    new Slack(config).postMessage(messages.join('\n----------\n'));
+    new Slack().postMessage(messages.reverse().join('\n----------\n'));
   } catch(e) {
     console.error(e);
   }
